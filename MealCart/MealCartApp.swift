e88ca6b90b9e4fa10628bce41973c0105e303cb7
Boyi Ma <boyima98@gmail.com>
2021-01-05 16:04:05 -0500

    Initial Commit

---
+++ MealCart/MealCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealCartApp.swift
+//  MealCart
+//
+//  Created by Boyi Ma on 2021-01-05.
+//
+
+import SwiftUI
+
+@main
+struct MealCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
