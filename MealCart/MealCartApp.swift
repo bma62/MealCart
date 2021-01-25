1fbe5c5e8a2fc6d2540bd87336a6181191d8241b
Boyi Ma <boyima98@gmail.com>
2021-01-24 20:31:21 -0500

    add Firebase SDKs to the project

--- MealCart/MealCartApp.swift
+++ MealCart/MealCartApp.swift
@@ -10,9 +10,13 @@
  */
 
 import SwiftUI
+import Firebase
 
 @main
 struct MealCartApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     // use StateObject to initialize a model object only once during the life time of the app
     // on contrast, ObservedObject is used only if that object is discarded after each use
     @StateObject private var modelData = ModelData()
@@ -23,4 +27,16 @@ struct MealCartApp: App {
                 .environmentObject(modelData)
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("App is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    
+    // Use Firebase library to configure APIs
+    FirebaseApp.configure()
+    
+    return true
+  }
 }
